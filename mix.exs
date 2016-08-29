1af72820687bd4b61eb00911eee362c50c2d5480
Eric <eric@Maggie.local>
2016-08-29 11:41:10 -0500

    Release v0.1.0

--- mix.exs
+++ mix.exs
@@ -1,13 +1,21 @@
 defmodule TrueStory.Mixfile do
   use Mix.Project
 
+  @version "0.1.0"
+
   def project do
     [app: :true_story,
-     version: "0.0.1",
+     version: @version,
      elixir: "~> 1.3-dev",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
-     deps: deps()]
+     deps: deps(),
+     description: description(),
+     package: package(),
+     name: "TrueStory",
+     docs: [source_ref: "v#{@version}",
+            source_url: "https://github.com/ericmj/true_story",
+            main: "readme", extras: ["README.md"]]]
   end
 
   # Configuration for the OTP application
@@ -15,6 +23,16 @@ defmodule TrueStory.Mixfile do
   # Type "mix help compile.app" for more information
   def application do
     [applications: [:logger]]
+  end
+
+  defp description do
+    "Make your tests tell a story"
+  end
+
+  defp package do
+    [maintainers: ["Eric Meadows-Jönsson", "Bruce Tate"],
+     licenses: ["Apache 2.0"],
+     links: %{"GitHub" => "https://github.com/ericmj/true_story"}]
   end
 
   # Dependencies can be Hex packages:
@@ -27,6 +45,6 @@ defmodule TrueStory.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    []
+    [{:ex_doc, ">= 0.0.0", only: :dev}]
   end
 end
